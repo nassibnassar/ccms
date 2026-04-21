44e020171bc96155495b7ac467bdbdccf61c7762
Nassib Nassar <nrn@etymon.com>
2026-04-20 22:39:00 -0400

    add parser for structured properties

---
+++ prop/prop.go
@@ -0,0 +1,28 @@
+package prop
+
+import "strings"
+
+// a name-title pair
+type Prop struct {
+	Name  string
+	Title string
+}
+
+// parse the value of a structured property into a slice of name-title pairs
+func Parse(rawProperty string) []Prop {
+	prop := make([]Prop, 0)
+	pairs := strings.Split(rawProperty, "|")
+	for i := range pairs {
+		p := strings.Split(pairs[i], ":")
+		l := len(p)
+		var name, title string
+		if l > 0 {
+			name = p[0]
+			if l > 1 {
+				title = p[1]
+			}
+		}
+		prop = append(prop, Prop{Name: name, Title: title})
+	}
+	return prop
+}
